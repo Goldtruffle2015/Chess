2397617eac9e12f1fa85192de9e9825612ad2860
John Yu <j.yu13@share.epsb.ca>
2019-03-04 09:04:51 -0700

    Create Chess Piece Sprites

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import math
 
 pygame.init() # Initialize Pygame
 
-windowx = 480
-windowy = 480
+windowx = 720
+windowy = 600
 window = pygame.display.set_mode((windowx, windowy)) # Creates the window
 pygame.display.set_caption('Chess')
 
@@ -25,15 +25,20 @@ class Squares():
 clock = pygame.time.Clock()
 
 ### --- Code Starts Here --- ###
+window.fill((200, 200, 200)) # Create Background
     ### --- Create the ChessBoard --- ###
 ChessBoard = []
-ChessBoardColors = [(255, 248, 220), (139, 69, 19)] # Colors: Cornsilk, Saddlebrown
+ChessBoardColors = [(255, 222, 173), (139, 69, 19)] # Colors: Navajowhite, Saddlebrown
 for i in range(64):
     x = (i % 8) * 60
     column = (i % 8)
-    y = math.floor(i / 8) * 60
+    y = (math.floor(i / 8) * 60) + 60
     row = math.floor(i / 8)
     ChessBoard.append(Squares(window, x, y, ChessBoardColors[(column + row) % 2]))
+
+    ### --- Create Control Panel --- ###
+        ### --- Background --- ###
+pygame.draw.rect(window, (255, 255, 255), pygame.Rect(485, 0, 235, 600))
 
 while True:
     clock.tick(60)
